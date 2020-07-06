284a80618107dba6e8414ace954272f272b607f9
shenkai001 <1679628460@qq.com>
2020-07-06 20:06:54 +0800

    当前提交了网页模板

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hello 龙傲天'
+    return render_template('home.html')
 
 if __name__="__main__":
 
