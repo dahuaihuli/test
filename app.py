37070a7933309576dea91cb5fd9f1c4a7e5097cd
shenkai001 <1679628460@qq.com>
2020-07-06 22:55:29 +0800

    添加了demo函数

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app=Flask(__name__)
 def hello():
     return render_template('home.html')
 
+@app.route('/demo')
+def demo():
+    return '叶良辰快快出来受死'
+
 if __name__="__main__":
 
     app.debug=True
